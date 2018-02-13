48e01182fce122dded6293c7a9cdd85113e7c566
unknown <jtc7537@gmail.com>
2018-02-12 23:18:34 -0500

    used the inquirer module for user searches

--- liri.js
+++ liri.js
@@ -2,15 +2,21 @@ require("dotenv").config();
 //What does .config() do?
 
 var Twitter = require('twitter');
-var Spotify = require('spotify');
+var Spotify = require('spotify-web-api-js');
+var request = require('request')
+var inquirer = require('inquirer')
 
 var command = process.argv[2];
+var search = process.argv[2];
 console.log(command)
 
 
 var keys = require('./keys.js')
 
-console.log(keys.spotify)
+var s = new Spotify()
+// var spotifyApi = new SpotifyWebApi();
+
+// console.log(keys.spotify)
 
 //var spotify = new Spotify(keys.spotify)
 //console.log(spotify)
@@ -19,8 +25,41 @@ var twitter = new Twitter(keys.twitter)
 
 // var spotify = new Spotify(keys.spotify)
 
+let searchTerm;
 
-if(command==='my-tweets'){
+inquirer.prompt([
+    {
+        type: 'list',
+        message: 'What are you looking for?',
+        choices: ['Tweets','Movie Data','Song Data'],
+        name: 'search'
+    }
+]).then(function(answer){
+    searchTerm = answer.search;
+    if(answer.search==='Tweets'){
+        tweets()
+    } else {
+        inquirer.prompt([
+            {
+            type: 'input',
+            name: 'query',
+            message: 'What would you like to search for?'    
+            }
+        ]).then(function(answer){
+            console.log(answer.search)
+            if(searchTerm==='Movie Data'){
+                console.log(answer.query)
+                movie(answer.query)
+            }
+            if(searchTerm==='Song Data'){
+                spotify(answer.query)
+            }
+        })
+    }
+})
+
+
+function tweets(){
     var params = {screen_name: 'Account for Class'}
     //not sure how get works
     //not sure how the parameters are being passed into the function
@@ -36,11 +75,25 @@ if(command==='my-tweets'){
 
     });
 }
-var spotifyKey = keys.spotify.id
-if(command==='spotify-this'){
 
-    Spotify.lookup({search: 'track', spotifyKey}, function(err, data){
-        console.log(err || data)
-        console.log(data)
-    })
+// spotifyApi.setAccessToken(`${keys.spotify.id}`)
+
+// function spotify(search){ 
+//     spotifyApi.searchTracks('love')
+//         .then(function(data){
+//             console.log
+//         }, function(error){
+//             console.log(error)
+//         })
+// }
+
+function movie(search){
+    request(`http://www.omdbapi.com/?t=${search}&apikey=trilogy`, function (error, response, body){
+    if(error){
+        console.log(error)
+    } else { 
+        var movie = JSON.parse(body)
+        console.log(movie)
+    }
+});
 }
